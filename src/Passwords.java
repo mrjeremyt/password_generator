bc2dc7fecc72810e02e5114272dc1e6c78a8cfcf
mrjeremyt <jthompson2214@gmail.com>
2014-11-13 16:10:49 -0600

    working on text parsing

--- src/Passwords.java
+++ src/Passwords.java
@@ -1,3 +1,9 @@
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.nio.file.Files;
+import java.util.Scanner;
+
 
 public class Passwords {
 
@@ -8,7 +14,24 @@ public class Passwords {
 	protected int[] COUNTS;
 	
 	public static void main(String[] args) {
-		System.out.println("lolz");
+		String[] parts = args[0].split("-");
+		String part2 = parts[1];
+		Scanner sc = null;
+		try {
+			sc = new Scanner(new File(part2));
+		} catch (FileNotFoundException e) {
+			System.out.println("wasn't able to open the reference text");
+			e.printStackTrace();
+		}
+		
+		parse_input(sc);
+	
+	}
+
+	private static void parse_input(Scanner sc) {
+		while(sc.hasNextLine()){
+			
+		}
 	}
 
 }
