bc3eb1e06339856ca4a787bc3e932d554fcd6d7c
mrjeremyt <jthompson2214@gmail.com>
2014-11-13 15:36:34 -0600

    we're starting!!!!!!

--- src/Passwords.java
+++ src/Passwords.java
@@ -1,6 +1,12 @@
 
 public class Passwords {
 
+	protected int num_passwords;
+	protected int password_length;
+	protected int[][] letter_grid;
+	protected int[] STARTERS;
+	protected int[] COUNTS;
+	
 	public static void main(String[] args) {
 		System.out.println("lolz");
 	}
