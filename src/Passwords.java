dc017cc691aeab26c9c7f1085038d05da5432d71
mrjeremyt <jthompson2214@gmail.com>
2014-11-13 16:51:09 -0600

    got the regex

--- src/Passwords.java
+++ src/Passwords.java
@@ -1,24 +1,25 @@
-import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.nio.file.Files;
 import java.util.Scanner;
 
 
 public class Passwords {
 
-	protected int num_passwords;
-	protected int password_length;
-	protected int[][] letter_grid;
-	protected int[] STARTERS;
-	protected int[] COUNTS;
+	protected static int num_passwords;
+	protected static int password_length;
+	protected static int[][] letter_grid;
+	protected static int[] STARTERS;
+	protected static int[] COUNTS;
 	
 	public static void main(String[] args) {
-		String[] parts = args[0].split("-");
-		String part2 = parts[1];
+		if(args.length != 3){
+			System.out.println("incorrect number of arguments. Please correct"); System.exit(-1);
+		}
 		Scanner sc = null;
 		try {
-			sc = new Scanner(new File(part2));
+			sc = new Scanner(new File(args[0].split("-")[1]));
+			num_passwords = Integer.parseInt(args[1]);
+			password_length = Integer.parseInt(args[2]);
 		} catch (FileNotFoundException e) {
 			System.out.println("wasn't able to open the reference text");
 			e.printStackTrace();
@@ -30,7 +31,13 @@ public class Passwords {
 
 	private static void parse_input(Scanner sc) {
 		while(sc.hasNextLine()){
-			
+			String line = sc.nextLine().replaceAll("[^A-Za-z]+", "");
+			Scanner l = new Scanner(line);
+			while(l.hasNext()){
+				String t = l.next().toLowerCase();
+				System.out.print(t);
+			}
+			System.out.println();
 		}
 	}
 
